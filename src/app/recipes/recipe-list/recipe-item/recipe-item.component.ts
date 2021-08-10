7c62f1218146efa067bed0cc8edfbbafa6d48942
Reid Vrana <reidvrana93@gmail.com>
2021-08-10 14:16:03 -0500

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,10 +8,17 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
+  onSelected(){
+    this.recipeSelected.emit();
+  }
+
   ngOnInit(): void {
   }
+
+  
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/995/747/603/recipe-dish-lunch-nutrition.jpg')
   ];
 
   constructor() { }
@@ -16,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe); 
+  }
 }
